2fdb082f2e1ce0a4264bee990125bf2c01f7b61c
zahraMajed <zahra_m.alzawad@hotmail.com>
2023-01-03 12:06:12 +0300

    Initial Commit

---
+++ NanoChallenge/NanoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallengeApp.swift
+//  NanoChallenge
+//
+//  Created by Zahra Majed Alzawad on 10/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
