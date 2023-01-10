ae8306d1eac6603786c6fb9f5b463a74df686cdf
zahraMajed <zahra_m.alzawad@hotmail.com>
2023-01-10 17:40:28 +0300

    ..

--- Bibli/Views/On Boardings/SplashView.swift
+++ Bibli/Views/On Boardings/SplashView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct SplashView: View {
+    //MARK: vars
+    @State private var isSplashActive = true
+    
     var body: some View {
-        GIFView(type: .name("BibliLogo"))
-            .frame(width: 390, height: 500)
+        
+        if isSplashActive {
+            GIFView(type: .name("BibliLogo"))
+                .frame(width: 390, height: 500)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                        self.isSplashActive = false
+                    }
+                }
+        } else {
+            ContentView()
+        }
     }
 }
 
